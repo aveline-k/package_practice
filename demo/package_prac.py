a0398822e0ac9a51d36fa8ad1d11b971f247bfe3
aveline-k <adk8cy@virginia.edu>
2023-03-28 20:22:20 -0400

    class practice

---
+++ demo/package_prac.py
@@ -0,0 +1,5 @@
+def practice(word:str):
+    print(word,'is a good word.')
+
+if __name__=='__main__':
+    practice('fine')
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='Demo package',
+      license='MIT',
+      packages=['demo'])
